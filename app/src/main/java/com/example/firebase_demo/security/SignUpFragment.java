ad535a6e3e78033133f20f0350c9151660861b09
zaid kamil <xaidmetamorphos@gmail.com>
2020-07-28 17:06:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firebase_demo/security/SignUpFragment.java
@@ -0,0 +1,63 @@
+package com.example.firebase_demo.security;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.firebase_demo.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class SignUpFragment extends Fragment {
+
+    private TextInputEditText editEmail;
+    private TextInputEditText editPass;
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View v, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(v, savedInstanceState);
+        editEmail = v.findViewById(R.id.editEmail);
+        editPass = v.findViewById(R.id.editPassword);
+        btnRegister = v.findViewById(R.id.btnRegister);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnRegister.setOnClickListener(view -> {
+            String email = editEmail.getText().toString();
+            String password = editPass.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(getActivity(), "regsitered", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getActivity(), "failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+}
